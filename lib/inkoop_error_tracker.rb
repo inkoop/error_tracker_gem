9f27c2c81a35d3a40f3aa3f19a483c113b804a51
Naveen <naveena.s.156@gmail.com>
2017-01-18 10:52:42 +0530

    uri added for error creating

--- lib/inkoop_error_tracker.rb
+++ lib/inkoop_error_tracker.rb
@@ -2,4 +2,16 @@ require "inkoop_error_tracker/version"
 
 module InkoopErrorTracker
   # Your code goes here...
+  class Tracker
+    
+    def self.api_key=(api_key)
+      @@api_key = api_key
+    end
+
+    def track
+      uri = URI('http://localhost:3000/errors')
+      res = Net::HTTP.post_form(uri, 'message' => 'mes', 'project_api' => @@api_key, 'description' => 'some description')
+    end
+
+  end
 end
