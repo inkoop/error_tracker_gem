307dd58ed2a86d8bb4d0d5dbb24d19bf91d8bbaf
Vivek <vivekpatna52@gmail.com>
2017-02-07 22:19:50 +0530

    gem updates

--- lib/inkoop_error_tracker.rb
+++ lib/inkoop_error_tracker.rb
@@ -1,18 +1,23 @@
 require "inkoop_error_tracker/version"
+require "net/http"
 
 module InkoopErrorTracker
   # Your code goes here...
   class Tracker
+
+    @@api_key = nil
     
     def self.api_key=(api_key)
       @@api_key = api_key
     end
 
     def self.track error
-      uri = URI('http://error.inkoop.in/errors')
-      message = "#{error.message} - #{error.backtrace[0]}"
-      description = error.backtrace.join('\n')
-      res = Net::HTTP.post_form(uri, 'message' => message, 'project_api' => @@api_key, 'description' => description)
+      if @@api_key
+        uri = URI('http://error.inkoop.in/errors')
+        message = "#{error.message} - #{error.backtrace[0]}"
+        description = error.backtrace.join('\n')
+        res = Net::HTTP.post_form(uri, 'message' => message, 'project_api' => @@api_key, 'description' => description)
+      end
     end
 
   end
