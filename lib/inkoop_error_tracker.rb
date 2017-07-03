bc41d5a8e20089b6a99b66079f521e9818a79bec
Vivek <vivekpatna52@gmail.com>
2017-07-04 00:30:44 +0530

    accepts parameters

--- lib/inkoop_error_tracker.rb
+++ lib/inkoop_error_tracker.rb
@@ -10,12 +10,18 @@ module InkoopErrorTracker
       @@api_key = api_key
     end
 
-    def self.track error
+    def self.track error, parameters
       if @@api_key
         uri = URI('http://error.inkoop.in/errors')
         message = "#{error.message} - #{error.backtrace[0]}"
         description = error.backtrace.join('\n')
-        res = Net::HTTP.post_form(uri, 'message' => message, 'project_api' => @@api_key, 'description' => description)
+        res = Net::HTTP.post_form(
+                uri, 
+                'message'     => message, 
+                'project_api' => @@api_key, 
+                'description' => description,
+                'parameters'  => parameters
+              )
       end
     end
 
--- lib/inkoop_error_tracker/version.rb
+++ lib/inkoop_error_tracker/version.rb
@@ -1,3 +1,3 @@
 module InkoopErrorTracker
-  VERSION = "1.1.4"
+  VERSION = "1.5.0"
 end
